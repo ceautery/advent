cc68ba17b0410b455e0c2e9c3d4365179a0e5080
Curtis Autery <curtis@rvshare.com>
2023-12-30 16:40:48 -0500

    More day 3 readability improvements

--- 2023/day03/day03.rb
+++ 2023/day03/day03.rb
@@ -10,29 +10,39 @@ class Day03
     matches = scan_lines_with_positions(/\d+/)
     symbols = scan_lines_with_positions(/[^.\d]/)
 
-    matches.select do |m|
-      symbols.any? { |s| s[:x].between?(m[:x]-1, m[:x] + m[:str].size) && s[:y].between?(m[:y] - 1, m[:y] + 1) }
-    end
-      .sum { |m| m[:str].to_i }
+    matches
+      .select { |m| symbols.any? { |s| m.near(s) } }
+      .sum(&:num)
   end
 
   def part_2
     matches = scan_lines_with_positions(/\d+/)
     stars = scan_lines_with_positions(/\*/)
 
-    connections = stars.map do |s|
-      matches.select { |m| s[:x].between?(m[:x]-1, m[:x] + m[:str].size) && s[:y].between?(m[:y] - 1, m[:y] + 1) }
-    end
-
-    connections.select { |c| c.size == 2 }.sum { |c| c[0][:str].to_i * c[1][:str].to_i }
+    stars
+      .map { |s| matches.select { |m| m.near(s) } }
+      .select { |c| c.size == 2 }.sum { |c| c.map(&:num).reduce(:*) }
   end
 
   def scan_lines_with_positions re
     lines.each_with_index.map do |line, line_num|
-      # Ruby peculiarity, this is the easiest way to collect MatchData objects during a scan
-      line.to_enum(:scan, re).map { Regexp.last_match }
-        .map { |md| { x: md.begin(0), y: line_num, str: md.to_s } }
+      line.to_enum(:scan, re).map { Match.new(Regexp.last_match, line_num) }
     end
       .flatten
+  end
+end
+
+class Match
+  attr_reader :x, :e, :y, :num
+
+  def initialize md, line_num
+    @x = md.begin(0)
+    @e = md.end(0)
+    @y = line_num
+    @num = md.to_s.to_i
+  end
+
+  def near other
+    other.x.between?(x - 1, e) && other.y.between?(y - 1, y + 1)
   end
 end
